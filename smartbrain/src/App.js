2590c4718c0829b754dba8eefa690952f9fae3aa
Supanut Watjanarat <supanut.wj@gmail.com>
2022-06-05 21:29:25 +0700

    Fix API and adding Face detection box

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -7,88 +7,28 @@ import Rank from './components/Rank/Rank.js';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import Clarifai from 'clarifai';
 
-// const app = new Clarifai.App({
-//   apiKey: '4b10b2f2167d4d8880055145ef157f6d'
-//  });
+const USER_ID = 'q6xintv0afbn';
+const PAT = 'b4ef32ecf3ca40f5b6a9c1dfb3d22d03';
+const APP_ID = '51c2397b0b544148a1e9b04c07069a21';
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
   }
 
-  onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
-
-  onButtonSubmit = (event) => {
-    console.log('click');
-    this.setState({imageUrl: this.state.input});
-    // app.models.predict(
-    //   Clarifai.COLOR_MODEL, 
-    //   'https://samples.clarifai.com/face-det.jpg')
-    //   .then(
-    //   function(response) {
-    //     console.log('response');
-
-    //   },
-    //   function(err) {
-
-
-    //   }
-    // );
-  }
-  
-
   particlesInit = async (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-
-
     await loadFull(main);
   };
 
-  particlesLoaded = (container) => {
-    console.log(container);
-  };
-
   particlesOptions = {
-      background: {
-        color: {
-          value: "#0d47a1",
-        },
-      },
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "push",
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-          resize: true,
-        },
-        modes: {
-          push: {
-            quantity: 4,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-        },
-      },
       particles: {
         color: {
           value: "#ffffff",
@@ -133,6 +73,66 @@ class App extends Component {
       detectRetina: true,
   }
 
+  apiData = () => {
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": this.state.input
+            }
+          }
+        }
+      ]
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+
+    return requestOptions;
+  }
+
+  // See not for using clarifai API: https://docs.clarifai.com/api-guide/predict/images
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    const requestOptions = this.apiData();
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response.text())
+    .then(result => this.displayFaceBox(this.calculateFaceLocation(JSON.parse(result))))
+    .catch(error => console.log('error', error));
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
   render() {
     return (
       <div className='App'>
@@ -149,7 +149,7 @@ class App extends Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
